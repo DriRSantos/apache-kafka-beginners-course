0a44e8398aa1786d2aae90b3b228ec52bce212fe
DriRSantos <drirsantos@outlook.com>
2023-07-29 15:40:12 -0300

    first commit

---
+++ kafka-beginners-course/kafka-basics/src/main/java/io/kafka/demos/udemy/ProducerDemo.java
@@ -0,0 +1,22 @@
+package io.kafka.demos.udemy;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class ProducerDemo {
+
+    private static final Logger log = LoggerFactory.getLogger(ProducerDemo.class.getSimpleName());
+
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+
+        // create Producer Properties
+
+        // create the Producer
+
+        // send data
+
+        // flush and close the producer
+
+    }
+}
