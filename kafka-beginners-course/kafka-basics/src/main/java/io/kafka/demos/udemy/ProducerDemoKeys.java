e13eecc7a36a0cd1ee0010cfed67506c265910b8
DriRSantos <drirsantos@outlook.com>
2023-07-29 19:08:54 -0300

    kafka producer java api with keys

---
+++ kafka-beginners-course/kafka-basics/src/main/java/io/kafka/demos/udemy/ProducerDemoKeys.java
@@ -0,0 +1,61 @@
+package io.kafka.demos.udemy;
+
+import org.apache.kafka.clients.producer.Callback;
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.kafka.clients.producer.RecordMetadata;
+import org.apache.kafka.common.serialization.StringSerializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.Properties;
+
+public class ProducerDemoKeys {
+
+    private static final Logger log = LoggerFactory.getLogger(ProducerDemoKeys.class.getSimpleName());
+
+    public static void main(String[] args) {
+        log.info("I am a Kafka Producer");
+
+        // create Producer Properties
+        Properties properties = new Properties();
+
+        properties.setProperty("bootstrap.servers", "[::1]:9092");
+        properties.setProperty("key.serializer", StringSerializer.class.getName());
+        properties.setProperty("value.serializer", StringSerializer.class.getName());
+
+        KafkaProducer<String, String> producer = new KafkaProducer<>(properties);
+
+        for (int j = 0; j < 2; j++) {
+            for (int i = 0; i < 10; i++) {
+
+                String topic = "demo_java";
+                String key = "id" + i;
+                String value = "hello world" + i;
+
+                // create a Producer Record
+                ProducerRecord<String, String> producerRecord =
+                        new ProducerRecord<>(topic, key, value);
+
+                // send data
+                producer.send(producerRecord, new Callback() {
+                    @Override
+                    public void onCompletion(RecordMetadata metadata, Exception e) {
+                        if (e == null) {
+                            log.info("Key: " + key + " | Partition: " + metadata.partition());
+                        } else {
+                            log.error("Error while producing", e);
+                        }
+                    }
+                });
+            }
+            try {
+                Thread.sleep(500);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+        }
+        producer.flush();
+        producer.close();
+    }
+}
