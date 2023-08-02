1a7fc0c04d4ae4871e993675731bb15c6a0b4e9b
DriRSantos <drirsantos@outlook.com>
2023-08-02 14:53:22 -0300

    set safe producer configs if needed

--- kafka-beginners-course/kafka-producer-wikimedia/src/main/java/io/kafka/demos/wikimedia/WikimediaChangesProducer.java
+++ kafka-beginners-course/kafka-producer-wikimedia/src/main/java/io/kafka/demos/wikimedia/WikimediaChangesProducer.java
@@ -1,7 +1,6 @@
 package io.kafka.demos.wikimedia;
+import com.launchdarkly.eventsource.EventHandler;
 import com.launchdarkly.eventsource.EventSource;
-import com.launchdarkly.eventsource.background.BackgroundEventHandler;
-import com.launchdarkly.eventsource.background.BackgroundEventSource;
 import org.apache.kafka.clients.producer.KafkaProducer;
 import org.apache.kafka.clients.producer.ProducerConfig;
 import org.apache.kafka.common.serialization.StringSerializer;
@@ -22,14 +21,24 @@ public class WikimediaChangesProducer {
         properties.setProperty(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
         properties.setProperty(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
 
+        // set safe producer configs (Kafka <=2.8)
+//        properties.setProperty(ProducerConfig.ENABLE_IDEMPOTENCE_CONFIG, "true");
+//        properties.setProperty(ProducerConfig.ACKS_CONFIG, "all"); // same as -1
+//        properties.setProperty(ProducerConfig.RETRIES_CONFIG, Integer.toString(Integer.MAX_VALUE));
+
         KafkaProducer<String, String> producer = new KafkaProducer<>(properties);
 
         String topic = "wikimedia.recentchange"; // Stream name that we are using
-        BackgroundEventHandler eventHandler = new WikimediaChangeHandler(producer, topic);
-        String url = "https://stream.wikimedia.org/v2/stream/recentchange"; // url Stream we are using
-        EventSource.Builder eventSourceBuilder = new EventSource.Builder(URI.create(url));
-        BackgroundEventSource.Builder builder = new BackgroundEventSource.Builder(eventHandler,eventSourceBuilder);
-        BackgroundEventSource eventSource = builder.build();
+//        BackgroundEventHandler eventHandler = new WikimediaChangeHandler(producer, topic);
+//        String url = "https://stream.wikimedia.org/v2/stream/recentchange"; // url Stream we are using
+//        EventSource.Builder eventSourceBuilder = new EventSource.Builder(URI.create(url));
+//        BackgroundEventSource.Builder builder = new BackgroundEventSource.Builder(eventHandler,eventSourceBuilder);
+//        BackgroundEventSource eventSource = builder.build();
+
+        EventHandler eventHandler = new WikimediaChangeHandler(producer, topic);
+        String url = "https://stream.wikimedia.org/v2/stream/recentchange";
+        EventSource.Builder builder = new EventSource.Builder(eventHandler, URI.create(url));
+        EventSource eventSource = builder.build();
 
         //start the producer in another thread
         eventSource.start();
